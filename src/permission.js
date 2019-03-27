43344066bc403db10d99ea537cc38be70296c937
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2019-03-27 23:48:26 +0800

    git init

---
+++ src/permission.js
@@ -0,0 +1,76 @@
+import router from './router'
+import store from './store'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+// import { Message } from 'element-ui'
+import { getToken } from '@/utils/auth' // getToken from cookie
+
+NProgress.configure({ showSpinner: false })// NProgress configuration
+
+const LOGIN_PAGE_NAME = 'Login'
+
+// 大概流程： 1. 在全局进行路由拦截。 -> 2. 判断是否获取了token
+
+// const whiteList = ['/login'] // 不重定向白名单
+// router.beforeEach((to, from, next) => {
+//   NProgress.start()
+//   if (getToken()) {
+//     if (to.path === '/login') {
+//       next({ path: '/' })
+//       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+//     } else {
+//       if (store.getters.roles.length === 0) {
+//         store.dispatch('GetInfo').then(res => { // 拉取用户信息
+//           next()
+//         }).catch((err) => {
+//           store.dispatch('FedLogOut').then(() => {
+//             Message.error(err || 'Verification failed, please login again')
+//             next({ path: '/' })
+//           })
+//         })
+//       } else {
+//         next()
+//       }
+//     }
+//   } else {
+//     if (whiteList.indexOf(to.path) !== -1) {
+//       next()
+//     } else {
+//       next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+//       NProgress.done()
+//     }
+//   }
+// })
+
+router.afterEach(() => {
+  // NProgress.done() // 结束Progress
+  console.log('route 跳转结束')
+})
+
+router.beforeEach((to, from, next) => {
+  const token = getToken()
+  if (token) {
+    if (to.name === LOGIN_PAGE_NAME) {
+      next({ path: '/' })
+    } else {
+      if (store.getters.roles.length === 0) {
+        store.dispatch('GetInfo').then(res => {
+          next()
+        }).catch(err => {
+          console.error(err)
+          store.dispatch('FedLogOut').then(res => {
+            next('/')
+          })
+        })
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (to.name === LOGIN_PAGE_NAME) {
+      next()
+    } else {
+      next({ name: LOGIN_PAGE_NAME })
+    }
+  }
+})
---
+++ src/views/login/index.vue
@@ -0,0 +1,213 @@
+<template>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+      <h3 class="title">vue-admin-template</h3>
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input v-model="loginForm.username" name="username" type="text" auto-complete="on" placeholder="username" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :type="pwdType"
+          v-model="loginForm.password"
+          name="password"
+          auto-complete="on"
+          placeholder="password"
+          @keyup.enter.native="handleLogin" />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="pwdType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+      <el-form-item>
+        <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">
+          Sign in
+        </el-button>
+      </el-form-item>
+      <div class="tips">
+        <span style="margin-right:20px;">username: admin</span>
+        <span> password: admin</span>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { isvalidUsername } from '@/utils/validate'
+
+export default {
+  name: 'Login',
+  data() {
+    // 自定义表单验证
+    const validateUsername = (rule, value, callback) => {
+      if (!isvalidUsername(value)) {
+        callback(new Error('请输入正确的用户名'))
+      } else {
+        callback()
+      }
+    }
+    const validatePass = (rule, value, callback) => {
+      if (value.length < 5) {
+        callback(new Error('密码不能小于5位'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      // 登陆表单数据
+      loginForm: {
+        username: 'admin',
+        password: 'admin'
+      },
+      // el-表单验证规则
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        password: [{ required: true, trigger: 'blur', validator: validatePass }]
+      },
+      // 加载状态标志
+      loading: false,
+      pwdType: 'password',
+      redirect: undefined
+    }
+  },
+  watch: {
+    $route: {
+      // vue-route 这一块看看！！
+      handler: function(route) {
+        console.log('route', route)
+        this.redirect = route.query && route.query.redirect
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    /**
+     * @description 是否显式的显示密码
+     */
+    showPwd() {
+      if (this.pwdType === 'password') {
+        this.pwdType = ''
+      } else {
+        this.pwdType = 'password'
+      }
+    },
+    /**
+     * @description 登陆按键事件
+     */
+    handleLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          this.$store.dispatch('Login', this.loginForm).then(() => {
+            this.loading = false
+            this.$router.push({ path: this.redirect || '/' })
+          }).catch(() => {
+            this.loading = false
+          })
+          // test
+          this.$store.dispatch('ActionTest', 'haha this is other data').then((res) => {
+            console.log('this is settime callback', res)
+          })
+          console.log('vuex get data', this.$store.getters['token'])
+        } else {
+          console.error('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+$bg:#2d3a4b;
+$light_gray:#eee;
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      &:-webkit-autofill {
+        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: #fff !important;
+      }
+    }
+  }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+
+</style>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+.login-container {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-color: $bg;
+  .login-form {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 520px;
+    max-width: 100%;
+    padding: 35px 35px 15px 35px;
+    margin: 120px auto;
+  }
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+  .title {
+    font-size: 26px;
+    font-weight: 400;
+    color: $light_gray;
+    margin: 0px auto 40px auto;
+    text-align: center;
+    font-weight: bold;
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
