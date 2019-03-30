147820e53de750dcb9aed1508846f4c2f597b9fa
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2019-03-30 08:47:33 +0800

    update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -5,24 +5,27 @@
       roles:
       <span v-for="role in roles" :key="role">{{ role }}</span>
     </div>
+    <img
+      src="https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3"
+      alt="hello"
+      class="wel-gif"
+    >
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"
+import { mapGetters } from "vuex";
 
 export default {
   name: "Dashboard",
   data() {
-    return {
-
-    }
+    return {};
   },
   computed: {
     ...mapGetters(["name", "roles"])
   },
   mounted() {
-    console.log('路由', this.$route);
+    console.log("路由", this.$route);
   }
 };
 </script>
@@ -30,11 +33,18 @@ export default {
 <style rel="stylesheet/scss" lang="scss" scoped>
 .dashboard {
   &-container {
-    margin: 30px;
+    padding: 40px 40px;
+    background-color: #e3e3e3;
+    min-height: 100vh;
   }
   &-text {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.wel-gif {
+  display: block;
+  margin: 0 auto;
+  width: 50%;
 }
 </style>
