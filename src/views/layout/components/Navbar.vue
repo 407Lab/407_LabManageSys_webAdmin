de72b89bc3df2a67ab9a7a0008cb952f7cb84bb8
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2019-07-19 23:58:36 +0800

    fix logout bug

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="navbar">
-    <hamburger :toggle-click="toggleSideBar" :is-active="sidebar.opened" class="hamburger-container"/>
+    <hamburger :toggle-click="toggleSideBar"
+               :is-active="sidebar.opened"
+               class="hamburger-container" />
     <breadcrumb />
-    <el-dropdown class="avatar-container" trigger="click">
+    <el-dropdown class="avatar-container"
+                 trigger="click">
       <div class="avatar-wrapper">
-        <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-        <i class="el-icon-caret-bottom"/>
+        <img :src="avatar+'?imageView2/1/w/80/h/80'"
+             class="user-avatar">
+        <i class="el-icon-caret-bottom" />
       </div>
-      <el-dropdown-menu slot="dropdown" class="user-dropdown">
-        <router-link class="inlineBlock" to="/">
+      <el-dropdown-menu slot="dropdown"
+                        class="user-dropdown">
+        <router-link class="inlineBlock"
+                     to="/">
           <el-dropdown-item>
             Home
           </el-dropdown-item>
         </router-link>
-        <el-dropdown-item divided>
-          <span style="display:block;" @click="logout">LogOut</span>
+        <el-dropdown-item>
+          <span style="display:block;"
+                @click.stop="logout">LogOut</span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
@@ -22,9 +29,9 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Breadcrumb from '@/components/Breadcrumb'
-import Hamburger from '@/components/Hamburger'
+import { mapGetters } from "vuex";
+import Breadcrumb from "@/components/Breadcrumb";
+import Hamburger from "@/components/Hamburger";
 
 export default {
   components: {
@@ -32,30 +39,34 @@ export default {
     Hamburger
   },
   computed: {
-    ...mapGetters([
-      'sidebar',
-      'avatar'
-    ])
+    ...mapGetters(["sidebar", "avatar"])
   },
   methods: {
     toggleSideBar() {
-      this.$store.dispatch('ToggleSideBar')
+      this.$store.dispatch("ToggleSideBar");
     },
     logout() {
-      this.$store.dispatch('LogOut').then(() => {
-        console.log('logout!!')
-        location.reload() // 为了重新实例化vue-router对象 避免bug
-      })
+      this.$store
+        .dispatch("FedLogOut")
+        .then(() => {
+          console.log(`/login?redirect=${this.$route.fullPath}`);
+          this.$router.push(`/login?redirect=${this.$route.fullPath}`);
+          location.reload(); // 为了重新实例化vue-router对象 避免bug
+        })
+        .catch(error => {
+          console.log("前端登出失败！！");
+          console.log(error);
+        });
     }
   }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .navbar {
   height: 50px;
   line-height: 50px;
-  box-shadow: 0 1px 3px 0 rgba(0,0,0,.12), 0 0 3px 0 rgba(0,0,0,.04);
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
   .hamburger-container {
     line-height: 58px;
     height: 50px;
